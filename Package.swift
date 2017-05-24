24c8d6e64a588658e6d6dedc43d01748bff18723
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-05-24 15:14:48 -0500

    Installed Vapor Core

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/vapor/console.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/json.git", majorVersion: 2)
+        .Package(url: "https://github.com/vapor/json.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/core.git", majorVersion: 2)
     ]
 )
