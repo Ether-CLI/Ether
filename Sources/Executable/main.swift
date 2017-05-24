4d6c8e7f3a5533b5f3e547d7739fba4500759650
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-05-24 11:18:30 -0500

    Built executable

--- Sources/Executable/main.swift
+++ Sources/Executable/main.swift
@@ -1,1 +1,21 @@
-print("Hello, world!")
+import Console
+import Haze
+
+var arguments = CommandLine.arguments
+
+let terminal = Terminal(arguments: arguments)
+
+var iterator = arguments.makeIterator()
+
+guard let executable = iterator.next() else {
+    throw ConsoleError.noExecutable
+}
+
+do {
+    try terminal.run(executable: executable, commands: [
+        Search(console: terminal)
+    ], arguments: Array(iterator),
+    help: [
+        "Need help? Open an issue on GitHub."
+    ])
+}
