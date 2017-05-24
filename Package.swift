7ea44267f03468f60db325ab57ded08c8116d590
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-05-24 08:51:00 -0500

    Project Creation

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+    name: "Haze",
+    targets: [
+        Target(name: "Haze"),
+        Target(name: "Executable", dependencies: ["Haze"])
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/vapor/console.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/json.git", majorVersion: 2)
+    ]
+)
