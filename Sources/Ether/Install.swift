cb4348e8b5d0e73b09dcea8028c09035d3dda019
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-09-15 16:57:22 -0500

    Fixed bug that failed install if the dependencies array didn't exist or was empty

--- Sources/Ether/Install.swift
+++ Sources/Ether/Install.swift
@@ -54,10 +54,14 @@ public final class Install: Command {
     }
     
     public func run(arguments: [String]) throws {
+        // Create and start the progress bar
         let installingProgressBar = console.loadingBar(title: "Installing Dependancy")
         installingProgressBar.start()
         
+        // Get the name of the package to install
         let name = try value("name", from: arguments)
+        
+        // Fetch the URL and version for the package from packagecatalog.com
         let (url, version) = try { ()throws -> (String, String) in
             var v = ""
             var u = ""
@@ -83,34 +87,69 @@ public final class Install: Command {
             return (u,v)
         }()
         
-        let versionNumbers = version.characters.split(separator: ".").map(String.init)
-        let regex = try NSRegularExpression(pattern: "(\\.Package([\\w\\s\\d\\,\\:\\(\\)\\@\\-\\\"\\/\\.])+\\))(?:\\R?)", options: .anchorsMatchLines)
-        
+        // Get the default FileManager instance
         let manager = FileManager.default
+        
+        // Check to make sure the Package.swift file exists
         if !manager.fileExists(atPath: "\(manager.currentDirectoryPath)/Package.swift") { throw EtherError.fail("There is no Package.swift file in the current directory") }
+        
+        // Get the data from the package manifest file
         let packageData = manager.contents(atPath: "\(manager.currentDirectoryPath)/Package.swift")
+        
+        // Get the number of packages installed before the install occurs
         let oldPins = try manager.contents(atPath: "\(manager.currentDirectoryPath)/Package.pins")?.json()?["pins"] as? [JSON]
         
+        // Convert the package manifets data to a String so we can read and modify it
         guard let packageString = String(data: packageData!, encoding: .utf8) else { throw fail(bar: installingProgressBar, with: "Unable to read Package.swift") }
+        
+        // The package manifets contents that will be changed based off of RegEx matches
         let mutableString = NSMutableString(string: packageString)
         
-        if regex.matches(in: packageString, options: [], range: NSMakeRange(0, packageString.utf8.count)).count == 0 {
-            throw fail(bar: installingProgressBar, with: "Make sure your Package.swift file is properly formatted! Does your last package have a trailing comma?")
+        // The version numbers for the package that will be installed
+        let versionNumbers = version.characters.split(separator: ".").map(String.init)
+        
+        // A RegEx pattern that will mach the packages in the dependencies array
+        let packageInstenceRegex = try NSRegularExpression(pattern: "(\\.Package([\\w\\s\\d\\,\\:\\(\\)\\@\\-\\\"\\/\\.])+\\))(?:\\R?)", options: .anchorsMatchLines)
+        
+        // Check to make sure that the dependencies array exists
+        if try NSRegularExpression(pattern: "(\\][\\n\\s]*,[\\n\\s]*|Package\\([\\n\\s]*name\\s*:\\s*\\\".*\\\"[\\n\\s]*,[\\n\\s]*)dependencies:[\\n\\s]*\\[(.|\\n)*\\]", options: []).matches(in: packageString, options: [], range: NSMakeRange(0, packageString.utf8.count)).count < 1 {
+            
+            // Create the dependencies array
+            try NSRegularExpression(pattern: "(Package\\([\\n\\s]*name\\s*:\\s*\\\".*\\\"[^,]|targets\\s*:\\s*\\[(\\n|.)*\\])", options: []).replaceMatches(in: mutableString, options: [], range: NSMakeRange(0, mutableString.length), withTemplate: "$1,\n    dependencies: [\n        \n    ]")
         }
         
-        regex.replaceMatches(in: mutableString, options: [], range: NSMakeRange(0, mutableString.length), withTemplate: "$1,\n        .Package(url: \"\(url)\", Version(\(versionNumbers[0]),\(versionNumbers[1]),\(versionNumbers[2])))\n")
+        // Check to see if there are packages in the dependencies array
+        if packageInstenceRegex.matches(in: packageString, options: [], range: NSMakeRange(0, packageString.utf8.count)).count != 0 {
+            
+            // Add the new package to the dependencies array
+            packageInstenceRegex.replaceMatches(in: mutableString, options: [], range: NSMakeRange(0, mutableString.length), withTemplate: "$1,\n        .Package(url: \"\(url)\", Version(\(versionNumbers[0]),\(versionNumbers[1]),\(versionNumbers[2])))\n")
+        } else {
+            
+            // Add the new package to the dependencies array
+            try NSRegularExpression(pattern: "\\],[\\s\\n]*dependencies:\\s*\\[", options: []).replaceMatches(in: mutableString, options: [], range: NSMakeRange(0, mutableString.length), withTemplate: "$1,\n        .Package(url: \"\(url)\", Version(\(versionNumbers[0]),\(versionNumbers[1]),\(versionNumbers[2])))\n")
+        }
+        
         do {
+            // Write the updated package information to the package manifest
             try String(mutableString).data(using: .utf8)?.write(to: URL(string: "file:\(manager.currentDirectoryPath)/Package.swift")!)
+            
+            // Update the project's packages
             _ = try console.backgroundExecute(program: "swift", arguments: ["package", "--enable-prefetching", "fetch"])
             _ = try console.backgroundExecute(program: "swift", arguments: ["package", "update"])
         } catch let error {
             installingProgressBar.fail()
             throw error
         }
+        
+        // Close the progress bar
         installingProgressBar.finish()
         if let pins = oldPins {
+            
+            // Get the new number of project dependencies
             if let newPins = try manager.contents(atPath: "\(manager.currentDirectoryPath)/Package.pins")?.json()?["pins"] as? [JSON] {
                 let newPackages = newPins.count - pins.count
+                
+                // Output the number of packages that got installed
                 console.output("📦  \(newPackages) packages installed", style: .custom(.white), newLine: true)
             }
         }
