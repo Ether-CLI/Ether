e0bbaf292840b81746cc226bc10699b8b08b9e34
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-09-15 16:59:02 -0500

    Updated version to 1.6.1

--- Sources/Executable/main.swift
+++ Sources/Executable/main.swift
@@ -25,7 +25,7 @@ import Ether
 import libc
 
 // The current version of Ether. This string should be updated with each release.
-let version = "1.6.0"
+let version = "1.6.1"
 var arguments = CommandLine.arguments
 let terminal = Terminal(arguments: arguments)
 
