b3e15188e257316a37f9eb5ada9ed44e50e27f32
Vitaly Popov <vit5711@yandex.ru>
2017-10-02 15:49:03 +0300

    updated to Swift 4

--- Package.swift
+++ Package.swift
@@ -1,17 +1,17 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
-let package = Package(
+Package(
     name: "Ether",
     targets: [
-        Target(name: "Helpers"),
-        Target(name: "Ether", dependencies: ["Helpers"]),
-        Target(name: "Executable", dependencies: ["Ether"])
+        .target(name: "Helpers"),
+        .target(name: "Ether", dependencies: ["Helpers"]),
+        .target(name: "Executable", dependencies: ["Ether"])
     ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/console.git", Version(2,2,0)),
-        .Package(url: "https://github.com/vapor/json.git", Version(2,2,0)),
-        .Package(url: "https://github.com/vapor/core.git", Version(2,1,2))
+        .package(url: "https://github.com/vapor/console.git", from: "2.2.0"),
+        .package(url: "https://github.com/vapor/json.git", from: "2.2.0"),
+        .package(url: "https://github.com/vapor/core.git", from: "2.1.2")
     ]
 )
