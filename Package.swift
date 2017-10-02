96fc5ad882a1167745d6f28767f8c2353dc5117c
Vitaly Popov <vit5711@yandex.ru>
2017-10-02 17:06:51 +0300

    Fix swift 4 migration
    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -2,16 +2,16 @@
 
 import PackageDescription
 
-Package(
+let package = Package(
     name: "Ether",
-    targets: [
-        .target(name: "Helpers"),
-        .target(name: "Ether", dependencies: ["Helpers"]),
-        .target(name: "Executable", dependencies: ["Ether"])
-    ],
     dependencies: [
         .package(url: "https://github.com/vapor/console.git", from: "2.2.0"),
         .package(url: "https://github.com/vapor/json.git", from: "2.2.0"),
         .package(url: "https://github.com/vapor/core.git", from: "2.1.2")
+    ],
+    targets: [
+        .target(name: "Helpers", dependencies: ["Core"]),
+        .target(name: "Ether", dependencies: ["Helpers", "Console"]),
+        .target(name: "Executable", dependencies: ["Ether"])
     ]
 )
