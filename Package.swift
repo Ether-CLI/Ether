2bf3625a662283ceb8ecd6dc7ddcc7698ac427e7
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-10-20 16:46:37 -0500

    Installed Vapor/JSON package

--- Package.swift
+++ Package.swift
@@ -7,11 +7,12 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/console.git", from: "2.2.0"),
         .package(url: "https://github.com/vapor/json.git", from: "2.2.0"),
-        .package(url: "https://github.com/vapor/core.git", from: "2.1.2")
+        .package(url: "https://github.com/vapor/core.git", from: "2.1.2"),
+        .package(url: "https://github.com/vapor/json", from: "2.2.0"),
     ],
     targets: [
-        .target(name: "Helpers", dependencies: ["Core"]),
-        .target(name: "Ether", dependencies: ["Helpers", "Console"]),
+        .target(name: "Helpers", dependencies: ["Core", "JSON"]),
+        .target(name: "Ether", dependencies: ["Helpers", "Console", "JSON"]),
         .target(name: "Executable", dependencies: ["Ether"])
     ]
 )
