a0c97e60d3b79cd732fd71939585ed80a1d5d315
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-10-24 15:51:35 -0500

    Updated executable version number to 1.7.0

--- Sources/Executable/main.swift
+++ Sources/Executable/main.swift
@@ -25,7 +25,7 @@ import Ether
 import libc
 
 // The current version of Ether. This string should be updated with each release.
-let version = "1.6.2"
+let version = "1.7.0"
 var arguments = CommandLine.arguments
 let terminal = Terminal(arguments: arguments)
 
