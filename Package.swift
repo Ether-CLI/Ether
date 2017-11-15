2eda61c4e6c09e1fb93c09398dca49da5b4c4cff
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-15 06:21:37 -0600

    Updated dependencies to latest versions

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 let package = Package(
     name: "Ether",
     dependencies: [
-        .package(url: "https://github.com/vapor/console.git", .exact("2.2.0")),
+        .package(url: "https://github.com/vapor/console.git", .exact("2.3.0")),
         .package(url: "https://github.com/vapor/json.git", .exact("2.2.1")),
-        .package(url: "https://github.com/vapor/core.git", .exact("2.1.2"))
+        .package(url: "https://github.com/vapor/core.git", .exact("2.2.0"))
     ],
     targets: [
         .target(name: "Helpers", dependencies: ["Core", "JSON"]),
