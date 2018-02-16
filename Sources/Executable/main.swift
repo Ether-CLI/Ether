05a23fa56101b5dfda915fb364e307faeffccd15
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-16 17:32:40 -0600

    Updated executable version

--- Sources/Executable/main.swift
+++ Sources/Executable/main.swift
@@ -27,7 +27,7 @@ import Helpers
 import libc
 
 // The current version of Ether. This string should be updated with each release.
-let version = "1.9.2"
+let version = "1.10.0"
 var arguments = CommandLine.arguments
 let terminal = Terminal(arguments: arguments)
 var iterator = arguments.makeIterator()
