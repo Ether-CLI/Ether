8880cd874ada70228b4ff665167d0952659c8a22
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-04 18:26:51 -0500

    Implemented Remove command signiture in Console 3

--- Sources/Ether/Remove.swift
+++ Sources/Ether/Remove.swift
@@ -1,51 +1,46 @@
-//// The MIT License (MIT)
-////
-//// Copyright (c) 2017 Caleb Kleveter
-////
-//// Permission is hereby granted, free of charge, to any person obtaining a copy
-//// of this software and associated documentation files (the "Software"), to deal
-//// in the Software without restriction, including without limitation the rights
-//// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//// copies of the Software, and to permit persons to whom the Software is
-//// furnished to do so, subject to the following conditions:
-////
-//// The above copyright notice and this permission notice shall be included in all
-//// copies or substantial portions of the Software.
-////
-//// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//// SOFTWARE.
+// The MIT License (MIT)
 //
-//import Helpers
-//import Console
-//import Foundation
+// Copyright (c) 2017 Caleb Kleveter
 //
-//public final class Remove: Command {
-//    public let id = "remove"
-//    
-//    public var help: [String] = [
-//        "Removes and uninstalls a package"
-//    ]
-//    
-//    public var signature: [Argument] = [
-//        Value(name: "name", help: [
-//            "The name of the package that will be removed"
-//        ]),
-//        Option(name: "xcode", short: "x", help: [
-//            "Regenerate the Xcode project after removing the package"
-//        ])
-//    ]
-//    
-//    public let console: ConsoleProtocol
-//    
-//    public init(console: ConsoleProtocol) {
-//        self.console = console
-//    }
-//    
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+import Console
+import Command
+import Async
+
+public final class Remove: Command {
+    public var arguments: [CommandArgument] = [
+        CommandArgument.argument(name: "name", help: ["The name of the package that will be removed"])
+    ]
+    
+    public var options: [CommandOption] = [
+        CommandOption.flag(name: "xcode", short: "x", help: ["Regenerate the Xcode project after removing the package"])
+    ]
+    
+    public var help: [String] = ["Removes a package from the manifest and uninstalls it"]
+    
+    public func run(using context: CommandContext) throws -> EventLoopFuture<Void> {
+        return context.container.eventLoop.newSucceededFuture(result: ())
+    }
+}
+    
 //    public func run(arguments: [String]) throws {
 //        let removingProgressBar = console.loadingBar(title: "Removing Dependency")
 //        removingProgressBar.start()
