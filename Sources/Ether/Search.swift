0404822d95b98cb0118f4a055f943bf5e37deb8c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-04 18:31:49 -0500

    Implemented Search command signiture in Console 3

--- Sources/Ether/Search.swift
+++ Sources/Ether/Search.swift
@@ -1,65 +1,56 @@
-//// The MIT License (MIT)
-////
-//// Copyright (c) 2017 Caleb Kleveter
-////
-//// Permission is hereby granted, free of charge, to any person obtaining a copy
-//// of this software and associated documentation files (the "Software"), to deal
-//// in the Software without restriction, including without limitation the rights
-//// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//// copies of the Software, and to permit persons to whom the Software is
-//// furnished to do so, subject to the following conditions:
-////
-//// The above copyright notice and this permission notice shall be included in all
-//// copies or substantial portions of the Software.
-////
-//// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//// SOFTWARE.
+// The MIT License (MIT)
 //
-//import Helpers
-//import Console
-//import Foundation
-//import Core
+// Copyright (c) 2017 Caleb Kleveter
 //
-//public final class Search: Command {
-//    public let id = "search"
-//    
-//    public let baseURL = "https://packagecatalog.com/api/search/"
-//    public let sort = "chart"
-//    public let results = "items"
-//    
-//    public var help: [String] = [
-//        "Searches for availible packages."
-//    ]
-//    
-//    public var signature: [Argument] = [
-//        Value(name: "name", help: [
-//            "The name of the package to search for."
-//        ]),
-//        Option(name: "max-results", help: [
-//            "The maximum number of results that will be returned.",
-//            "This defaults to 20."
-//        ]),
-//        Option(name: "sort", help: [
-//            "The sorting method to use:",
-//            "moststarred (Most Starred)",
-//            "leaststarred (Least Starred)",
-//            "mostrecent (Most Recent)",
-//            "leastrecent (Least Recent)",
-//            "The default value is moststarred."
-//        ])
-//    ]
-//    
-//    public let console: ConsoleProtocol
-//    public let client = PackageJSONFetcher()
-//    
-//    public init(console: ConsoleProtocol) {
-//        self.console = console
-//    }
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Console
+import Command
+import Async
+
+public final class Search: Command {
+    public var arguments: [CommandArgument] = [
+        CommandArgument.argument(name: "name", help: ["The name of the package to search for."])
+    ]
+    
+    public var options: [CommandOption] = [
+        CommandOption.value(name: "max-results", default: "20", help: [
+                "The maximum number of results that will be returned.",
+                "This defaults value 20."
+            ]),
+        CommandOption.value(name: "sort", default: "moststarred", help: [
+                "The sorting method to use:",
+                "moststarred (Most Starred)",
+                "leaststarred (Least Starred)",
+                "mostrecent (Most Recent)",
+                "leastrecent (Least Recent)",
+                "The default value is moststarred."
+            ])
+    ]
+    
+    public var help: [String] = ["Searches for availible packages."]
+    
+    public func run(using context: CommandContext) throws -> EventLoopFuture<Void> {
+        return context.container.eventLoop.newSucceededFuture(result: ())
+    }
+}
+
 //    
 //    public func run(arguments: [String]) throws {
 //        let searchingBar = console.loadingBar(title: "Searching")
