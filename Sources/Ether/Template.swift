0a29a33fbb1f19a4c51a9a916cf64d3a1f8ab2bf
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-04 18:35:59 -0500

    Implemented Template command signiture in Console 3

--- Sources/Ether/Template.swift
+++ Sources/Ether/Template.swift
@@ -1,57 +1,48 @@
-//// The MIT License (MIT)
-////
-//// Copyright (c) 2017 Caleb Kleveter
-////
-//// Permission is hereby granted, free of charge, to any person obtaining a copy
-//// of this software and associated documentation files (the "Software"), to deal
-//// in the Software without restriction, including without limitation the rights
-//// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//// copies of the Software, and to permit persons to whom the Software is
-//// furnished to do so, subject to the following conditions:
-////
-//// The above copyright notice and this permission notice shall be included in all
-//// copies or substantial portions of the Software.
-////
-//// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//// SOFTWARE.
+// The MIT License (MIT)
 //
-//// Path: ~/Library/Application\ Support/Ether
+// Copyright (c) 2017 Caleb Kleveter
 //
-//import Console
-//import Foundation
-//import Core
-//import Helpers
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-//final public class Template: Command {
-//    public let id = "template"
-//    
-//    public let signature: [Argument] = [
-//        Value(name: "template-name", help: [
-//            "The name used to identify the template"
-//        ]),
-//        Option(name: "github", help: [
-//            "Creates a GitHub repo and pushes the template to it (Un-implimented)"
-//        ]),
-//        Option(name: "remove", short: "r", help: [
-//            "Deletes the template"
-//        ])
-//    ]
-//    
-//    public let help: [String] = [
-//        "Creates and stores a template for use as the starting point of a project."
-//    ]
-//    
-//    public let console: ConsoleProtocol
-//    
-//    public init(console: ConsoleProtocol) {
-//        self.console = console
-//    }
-//    
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Path: ~/Library/Application\ Support/Ether
+
+import Console
+import Command
+import Async
+
+public final class Template: Command {
+    public var arguments: [CommandArgument] = [
+        CommandArgument.argument(name: "name", help: ["The name used to identify the template"])
+    ]
+    
+    public var options: [CommandOption] = [
+        CommandOption.flag(name: "remove", short: "r", help: ["Deletes the template"])
+        // TODO: Add `github` flag to create remote repo and push.
+    ]
+    
+    public var help: [String] = ["Creates and stores a template for use as the starting point of a project."]
+    
+    public func run(using context: CommandContext) throws -> EventLoopFuture<Void> {
+        return context.container.eventLoop.newSucceededFuture(result: ())
+    }
+}
+
 //    public func run(arguments: [String]) throws {
 //        let name = try value("template-name", from: arguments)
 //        let useGitHub = arguments.option("github") != nil ? true : false
