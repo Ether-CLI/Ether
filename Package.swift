fdbf2120cae185ead933787b117f41653242c921
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-05 15:21:22 -0500

    Updated Console version to 3.0.0

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
     name: "Ether",
     dependencies: [
-        .package(url: "https://github.com/vapor/console.git", from: "3.0.0-rc"),
-        .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/vapor/console.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/core.git", from: "3.0.0"),
         .package(url: "https://github.com/Ether-CLI/Manifest.git", from: "0.1.0")
     ],
     targets: [
