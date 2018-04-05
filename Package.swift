25cc787bde2684bcc1d23b12305f67e873e133c6
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-05 07:50:40 -0500

    Installed Manifest package

--- Package.swift
+++ Package.swift
@@ -6,12 +6,13 @@ let package = Package(
     name: "Ether",
     dependencies: [
         .package(url: "https://github.com/vapor/console.git", from: "3.0.0-rc"),
-        .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc")
+        .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/Ether-CLI/Manifest.git", from: "0.1.0")
     ],
     targets: [
         .target(name: "GitHub", dependencies: ["Core"]),
         .target(name: "Helpers", dependencies: ["Core", "Console"]),
-        .target(name: "Ether", dependencies: ["Helpers", "Console", "Command"]),
+        .target(name: "Ether", dependencies: ["Helpers", "Console", "Command", "Manifest"]),
         .target(name: "Executable", dependencies: ["Ether", "Console"])
     ]
 )
