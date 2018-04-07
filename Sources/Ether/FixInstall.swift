864d1e0ae1a544e9a208bf05fe124873af5193c1
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-07 00:05:15 -0500

    Ported FixInstall command to Console 3

--- Sources/Ether/FixInstall.swift
+++ Sources/Ether/FixInstall.swift
@@ -1,60 +1,47 @@
-//// The MIT License (MIT)
-////
-//// Copyright (c) 2017 Caleb Kleveter
-////
-//// Permission is hereby granted, free of charge, to any person obtaining a copy
-//// of this software and associated documentation files (the "Software"), to deal
-//// in the Software without restriction, including without limitation the rights
-//// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//// copies of the Software, and to permit persons to whom the Software is
-//// furnished to do so, subject to the following conditions:
-////
-//// The above copyright notice and this permission notice shall be included in all
-//// copies or substantial portions of the Software.
-////
-//// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//// SOFTWARE.
+// The MIT License (MIT)
 //
-//import Console
+// Copyright (c) 2017 Caleb Kleveter
 //
-//public class FixInstall: Command {
-//    public let id: String = "fix-install"
-//    
-//    public let signature: [Argument] = [
-//        Option(name: "no-build", help: [
-//            "Skips the rebuilding proccess after clearing the project cache"
-//        ])
-//    ]
-//    
-//    public var help: [String] = [
-//        "Fixes fetching errors that occur during package install"
-//    ]
-//    
-//    public let console: ConsoleProtocol
-//    
-//    public init(console: ConsoleProtocol) {
-//        self.console = console
-//    }
-//    
-//    public func run(arguments: [String]) throws {
-//        let fixBar = console.loadingBar(title: "Fixing Installation")
-//        
-//        console.output("This may take some time...", style: .info, newLine: true)
-//        fixBar.start()
-//        
-//        _ = try console.backgroundExecute(program: "rm", arguments: ["-rf", ".build"])
-//        _ = try console.backgroundExecute(program: "swift", arguments: ["package", "update"])
-//        _ = try console.backgroundExecute(program: "swift", arguments: ["package", "resolve"])
-//        
-//        if arguments.option("no-build") == nil {
-//            _ = try console.backgroundExecute(program: "swift", arguments: ["build"])
-//        }
-//        
-//        fixBar.finish()
-//    }
-//}
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Console
+import Command
+import Async
+
+public final class FixInstall: Command {
+    public var arguments: [CommandArgument] = []
+    public var options: [CommandOption] = []
+    
+    public var help: [String] = ["Fixes fetching errors that occur during package install"]
+    
+    public func run(using context: CommandContext) throws -> EventLoopFuture<Void> {
+        context.console.output("This may take some time...", style: .info)
+        
+        let fixing = context.console.loadingBar(title: "Fixing Instillation")
+        _ = fixing.start(on: context.container)
+        
+        _ = try Process.execute("rm", ["--rf", ".build"])
+        _ = try Process.execute("swift", ["package", "update"])
+        _ = try Process.execute("swift", ["package", "resolve"])
+        
+        fixing.succeed()
+        
+        return context.container.eventLoop.newSucceededFuture(result: ())
+    }
+}
