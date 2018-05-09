6cd580da421ae4af79857cd51b70f541415ffbc0
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-09 17:15:29 -0500

    Installed Vapor package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Ether",
     dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/console.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/core.git", from: "3.0.0"),
         .package(url: "https://github.com/Ether-CLI/Manifest.git", from: "0.1.0")
@@ -12,6 +13,6 @@ let package = Package(
     targets: [
         .target(name: "Helpers", dependencies: ["Core", "Console"]),
         .target(name: "Ether", dependencies: ["Helpers", "Console", "Command", "Manifest", "Core"]),
-        .target(name: "Executable", dependencies: ["Ether", "Console"])
+        .target(name: "Executable", dependencies: ["Vapor", "Ether", "Console"])
     ]
 )
