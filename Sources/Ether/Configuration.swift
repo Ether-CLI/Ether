03d4103dfaec9df8b5818f066e4951a994580b34
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-25 10:11:26 -0500

    Created signed-commits key for Config struct

--- Sources/Ether/Configuration.swift
+++ Sources/Ether/Configuration.swift
@@ -34,7 +34,8 @@ public class Configuration: Command {
             "Valid keys are:",
             "- access-token: The GitHub access token to use for interacting the the GraphQL API. You can create on at https://github.com/settings/token",
             "- install-commit: The commit message to use on package install. Use &0 as package name placeholder",
-            "- remove-commit: The commit message to use on when a package is removed. Use &0 as package name placeholder"
+            "- remove-commit: The commit message to use on when a package is removed. Use &0 as package name placeholder",
+            "- signed-commits: If set to a truthy value (true, yes, y, 1), auto-commits will pass in the '-S' flag"
         ]),
         CommandArgument.argument(name: "value", help: ["The new value for the key passed in. If no value is passed in, the key will be removed from the config"])
     ]
@@ -100,11 +101,13 @@ public struct Config: Codable, Reflectable {
     public var accessToken: String?
     public var installCommit: String?
     public var removeCommit: String?
+    public var signedCommits: String?
     
     static let properties: [String: WritableKeyPath<Config, String?>] = [
         "access-token": \.accessToken,
         "install-commit": \.installCommit,
-        "remove-commit": \.removeCommit
+        "remove-commit": \.removeCommit,
+        "signed-commits": \.signedCommits
     ]
     
     func token()throws -> String {
